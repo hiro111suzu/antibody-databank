2ed91d80f4cd43f40af86ca6478f846e2923eb00
Hirofumi Suzuki <hirofumi@novemwin.localdomain>
2019-09-06 17:33:39 +0900

    add new file

---
+++ index.php
@@ -0,0 +1,1 @@
+﻿<?php //phpinfo();require_once( __DIR__ . '/common-web.php' );_redirect( 'display.php' );//echo "hoge";/*_simple_out([	'title' => 'Antibody Databank' ,	'sub'	=> 'Information repository for antibodies and suppliers' ,	'css' 	=> [ 'emn' ] ,	'icon'	=> 'white']);*/
