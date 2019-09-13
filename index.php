7a439c486510b23f89c3efa8623a2ec01832cee7
Hirofumi Suzuki <hirofumi@novemwin.localdomain>
2019-09-13 18:26:19 +0900

    line feed code

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-﻿<?php //phpinfo();ini_set( 'display_errors', '1' );require_once( __DIR__ . '/common-web.php' );_redirect( 'display.php' );//echo "hoge";/*_simple_out([	'title' => 'Antibody Databank' ,	'sub'	=> 'Information repository for antibodies and suppliers' ,	'css' 	=> [ 'emn' ] ,	'icon'	=> 'white']);*///- testtest
+﻿<?php 
+//phpinfo();
+ini_set( 'display_errors', '1' );
+require_once( __DIR__ . '/common-web.php' );
+
+_redirect( 'display.php' );
+//echo "hoge";
+
+/*
+_simple_out([
+	'title' => 'Antibody Databank' ,
+	'sub'	=> 'Information repository for antibodies and suppliers' ,
+	'css' 	=> [ 'emn' ] ,
+	'icon'	=> 'white'
+]);
+*/
+//- testtest
